1a4017cfec6739a17987e781666470a0849539c7
Alzater <alzater@mail.ru>
2015-02-08 15:59:55 +0300

    admin cofig

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,8 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Ban"
+  config.site_title = "Bear corner"
+  config.comments = false
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
