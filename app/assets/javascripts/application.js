b08104296850165aecf53689a2409adc09537a9c
Alzater <alzater@mail.ru>
2016-02-10 22:49:42 +0300

    better photos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery.fancybox.pack.js
+//= require jquery.blueimp-gallery.min
+//= require bootstrap-image-gallery.min
 //= require_tree .
